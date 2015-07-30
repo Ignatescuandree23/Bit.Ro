5b868d9b96a1d656339baae5ac38640f09f0c8d4
Ignatescuandree23 <ignatescuandree23@gmail.com>
2015-07-30 22:31:07 +0300

    update

---
+++ Assets/PlayerController23.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController23 : MonoBehaviour {
+	
+	public float speed;
+	public Text countText;
+	public Text winText;
+	
+	private Rigidbody rb;
+	private int count;
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rb.AddForce (movement * speed);
+	}
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ( "Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 2;
+			SetCountText ();
+		}
+		if (other.gameObject.CompareTag ( "Pick Up2"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+		if (other.gameObject.CompareTag ( "Boom"))
+		{
+			other.gameObject.SetActive (false);
+			count = count - 3;
+			SetCountText ();
+		}
+		if (other.gameObject.CompareTag ( "Pick Up3"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 45 ;
+			SetCountText ();
+		}
+	}
+	
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 100)
+		{
+			winText.text = "Felicitari, ai trecut la nivelul urmator!~ ";
+			int i = Application.loadedLevel;
+			Application.LoadLevel(i + 1);
+		}
+	}
+	
+}
